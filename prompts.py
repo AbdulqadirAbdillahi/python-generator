7cef9e1e3683c54c554851e5c6a87d935ba3d3b5
Abdulqadir Abdillahi <abdiqaadirabdillahi@gmail.com>
2026-02-22 00:03:35 +0000

    prompts completed

---
+++ prompts.py
@@ -0,0 +1,20 @@
+from inquirerPy import prompt
+
+# list of prompts for the user
+
+quiz_questions = [
+    {"type": "input", "name": "project_title", "message": "Enter the project title:"},
+    {"type": "input", "name": "project_desc", "message": "Brief description of the project:"},
+    {"type": "input", "name": "install_steps", "message": "Installation steps:"},
+    {"type": "input", "name": "usage_info", "message": "How to use this project:"},
+    {
+        "type": "list",
+        "name": "project_license",
+        "message": "Select a license for your project:",
+        "choices": ["MIT", "Apache 2.0", "GPLv3", "Unlicense"],
+    },
+    {"type": "input", "name": "author_name", "message": "Your name:"},
+    {"type": "input", "name": "contact_info", "message": "How to contact you:"},
+
+
+]
